83207bbcd386543c63315481eb57fcd230d1f58b
Milos Bastajic <milos.bastajic98@gmail.com>
2019-10-01 11:18:33 +0200

    Updated Ads-class and fxml for it, added css file for ads

--- bYMe/src/main/java/Model/Ads.java
+++ bYMe/src/main/java/Model/Ads.java
@@ -1,4 +1,37 @@
 package Model;
 
 public class Ads {
+    private String title;
+    private int price;
+    private String description;
+    private String location;
+    private String imagePath;
+
+    public Ads(String title, int price, String description, String location, String imagePath) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.location = location;
+        this.imagePath = imagePath;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
 }
