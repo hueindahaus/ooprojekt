1001b8955912e46be4d7284a9dcaef391587bf00
joeljnsn <joejons@student.chalmers.se>
2019-10-02 16:03:57 +0200

    Added json as a dependency in maven
    Added a new logo and a alternative image when you hover over it

--- bYMe/src/main/java/Controller/MainController.java
+++ bYMe/src/main/java/Controller/MainController.java
@@ -6,6 +6,7 @@ import Services.AdHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import java.net.URL;
 import java.util.ResourceBundle;
