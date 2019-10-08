04b0a045074b3bebc2f9eb68fc8467172cef7481
Johan Gottlander <johangottlander@gmail.com>
2019-10-08 20:20:05 +0200

    Implemented an edit mode for ads.
    Main changes: Implemented the ability for a user to edit their ads and save the changes.
    Implemented Observer pattern so that an ad can notify the view when is has been changed.
    
    Minor changes:
    Opacity in css for the exitButton.
    Wrote somme javadocs in DetailViewController.

--- bYMe/src/main/java/Controller/DetailViewController.java
+++ bYMe/src/main/java/Controller/DetailViewController.java
@@ -2,23 +2,31 @@ package Controller;
 
 import Model.Ad;
 import Model.Byme;
+import Model.IObserver;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-public class DetailViewController extends AnchorPane {
+public class DetailViewController extends AnchorPane{
 
 
     DetailViewToggler detailViewToggler;
 
     Byme byme = Byme.getInstance();
 
+
     @FXML
     Button deleteButton;
+    @FXML
+    Button editButton;
+    @FXML
+    Button saveButton;
     @FXML
     Label adTitle;
     @FXML
@@ -29,6 +37,17 @@ public class DetailViewController extends AnchorPane {
     Label adUser;
     @FXML
     Label adPrice;
+    @FXML
+    TextField adTitleTextField;
+    @FXML
+    TextField adLocationTextField;
+    @FXML
+    TextField adDescriptionTextField;
+    @FXML
+    TextField adUserTextField;
+    @FXML
+    TextField adPriceTextField;
+
     @FXML
     AnchorPane greyZone;
 
@@ -47,10 +66,15 @@ public class DetailViewController extends AnchorPane {
 
 
         this.detailViewToggler = detailViewToggler;
+
+
     }
 
 
-
+    /**
+     *
+     * @param ad Sends in an ad as parameter so the detailView can show the data of an ad.
+     */
     void setAd(Ad ad){
         if (ad != null) {
             this.ad = ad;
@@ -62,12 +86,19 @@ public class DetailViewController extends AnchorPane {
         }
     }
 
+
+    /**
+     * Used for the exitButton which upon a press closes the detailView panel.
+     */
     @FXML
     void closeDetailView(){
         detailViewToggler.toggleDetailView(false, ad);
     }
 
 
+    /**
+     * Gives the user the ability to remove a specific ad.
+     */
     @FXML
     void removeAd(){
         byme.removeAd(ad.getAdId());
@@ -75,14 +106,65 @@ public class DetailViewController extends AnchorPane {
     }
 
 
+    /**
+     * Used for the button which upon a press enables the "edit mode".
+     */
+    @FXML
+    void editAd(){
 
+        showTextFields();
+
+        editButton.setVisible(false);
+        saveButton.setVisible(true);
+
+    }
+
+
+    /**
+     * Saves the changes when a user has edited an ad.
+     * It also notifies observers who has registered to listen to these changes via the
+     * observerble "ad".
+     */
+    @FXML
+    void saveChanges(){
+
+        ad.setTitle(adTitleTextField.getText());
+        ad.setDescription(adDescriptionTextField.getText());
+        ad.setLocation(adLocationTextField.getText());
+        ad.setPrice(Integer.valueOf(adPriceTextField.getText()));
+
+        adTitle.setText(ad.getTitle());
+        adLocation.setText(ad.getLocation());
+        adDescription.setText(ad.getDescription());
+        adPrice.setText(String.valueOf(ad.getPrice()));
+
+
+
+        showLabels();
+
+
+        editButton.setVisible(true);
+        saveButton.setVisible(false);
+
+        ad.notifyMethod();
+
+    }
+
+
+    /**
+     * Method that makes sure that the delete and edit button only shows on an ad if the logged in user
+     * owns that specific ad.
+     */
     void showUserButtons(){
         if(isUsersAd()){
             deleteButton.setVisible(true);
+            editButton.setVisible(true);
         } else {
             deleteButton.setVisible(false);
+            editButton.setVisible(false);
         }
     }
+
 
 
     private boolean isUsersAd(){
@@ -93,5 +175,50 @@ public class DetailViewController extends AnchorPane {
         }
         return false;
     }
+
+    /**
+     * Method which enables visibility for an ads TextFields and at the same time disables visibility for the Labels.
+     * It also sets the text for all TextFields so they get the current data.
+     *
+     */
+    void showTextFields(){
+
+        adTitle.setVisible(false);
+        adLocation.setVisible(false);
+        adDescription.setVisible(false);
+        adPrice.setVisible(false);
+
+        adTitleTextField.setVisible(true);
+        adLocationTextField.setVisible(true);
+        adDescriptionTextField.setVisible(true);
+        adPriceTextField.setVisible(true);
+
+        adTitleTextField.setText(adTitle.getText());
+        adLocationTextField.setText(adLocation.getText());
+        adDescriptionTextField.setText(adDescription.getText());
+        adPriceTextField.setText(adPrice.getText());
+
+    }
+
+    /**
+     * Method which enables visibility for an ads Labels and at the same time disables visibility for the TextFields.
+     *
+     */
+
+    public void showLabels(){
+
+        adTitle.setVisible(true);
+        adLocation.setVisible(true);
+        adDescription.setVisible(true);
+        adPrice.setVisible(true);
+
+        adTitleTextField.setVisible(false);
+        adLocationTextField.setVisible(false);
+        adDescriptionTextField.setVisible(false);
+        adPriceTextField.setVisible(false);
+
+    }
+
+
 
 }
