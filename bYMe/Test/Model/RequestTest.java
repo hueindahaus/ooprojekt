f4a1d82bd43707203f090c244404576ff5044a6b
Adam Jawad <abbiefredriksson@gmail.com>
2019-10-17 14:04:05 +0200

    More tests added from DevAdam.
    Tests for bYMe methods and AdTest

---
+++ bYMe/Test/Model/RequestTest.java
@@ -0,0 +1,24 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestTest {
+
+    @Test
+    void getSender() {
+    }
+
+    @Test
+    void getReceiver() {
+    }
+
+    @Test
+    void getAd() {
+    }
+
+    @Test
+    void getMessage() {
+    }
+}
