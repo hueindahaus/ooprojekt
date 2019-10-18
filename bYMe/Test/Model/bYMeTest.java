56989ca6592d6f7c1d430bbbc6841f2492e647bd
Milos <milos.bastajic98@gmail.com>
2019-10-18 12:13:56 +0200

    Auto stash before merge of "devMB" and "origin/devMB"

--- bYMe/Test/Model/bYMeTest.java
+++ bYMe/Test/Model/bYMeTest.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.*;
 
 class bYMeTest {
-
+/*
     @Test
     void registerAccount() {
         IAccountHandler accountHandler = new IAccountHandler() {
